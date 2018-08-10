2f985e0ac546cc2292b9f4dd9898eb5221727377
Dennis Zierahn <dezi@kappa-mm.de>
2018-08-10 14:11:15 +0200

    Check in.

---
+++ GorillaMessenger/app/src/main/java/com/aura/aosp/gorilla/messenger/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aura.aosp.gorilla.messenger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
