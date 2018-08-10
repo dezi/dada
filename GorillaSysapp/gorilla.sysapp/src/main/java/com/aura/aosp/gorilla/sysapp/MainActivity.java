89f251b0b7219bb2fe1cea0413eedc8165c2a0b4
Dennis Zierahn <dezi@kappa-mm.de>
2018-08-10 14:17:33 +0200

    Check in.

---
+++ GorillaSysapp/gorilla.sysapp/src/main/java/com/aura/aosp/gorilla/sysapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.aura.android.gorilla.sysapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity
+{
+    private static final String LOGTAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        Log.d(LOGTAG, "onCreate: activity started.");
+    }
+}
