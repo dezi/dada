0f22aca1a5e72360468b357ccd2fef408c0d84b9
Dennis Zierahn <dezi@kappa-mm.de>
2018-09-13 17:20:16 +0200

    Simplyfied SHA generation.

--- Modules/gorilla.service/src/main/java/com/aura/aosp/gorilla/service/GorillaSystemService.java
+++ Modules/gorilla.service/src/main/java/com/aura/aosp/gorilla/service/GorillaSystemService.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
 
-import com.aura.aosp.aura.common.crypter.SHA;
 import com.aura.aosp.aura.common.univid.Owner;
 import com.aura.aosp.aura.common.simple.Log;
 
