28cb252d0813947e7481df349e951f79238a6527
Dennis Zierahn <dezi@kappa-mm.de>
2018-10-08 13:00:51 +0200

    Service cron job.

--- Modules/gorilla.service/src/main/java/com/aura/aosp/gorilla/service/GorillaBase.java
+++ Modules/gorilla.service/src/main/java/com/aura/aosp/gorilla/service/GorillaBase.java
@@ -3,10 +3,15 @@ package com.aura.aosp.gorilla.service;
 import android.support.annotation.NonNull;
 
 import android.app.Application;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
 
 import com.aura.aosp.aura.common.simple.Err;
 import com.aura.aosp.aura.common.simple.Log;
 import com.aura.aosp.aura.common.simple.Simple;
+
 import com.aura.aosp.gorilla.gomess.GomessHandler;
 
 public class GorillaBase extends Application
@@ -30,24 +35,54 @@ public class GorillaBase extends Application
 
         Simple.initialize(this);
 
-        Log.d("######################");
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M)
-        {
-            Log.d("######################");
-
-            Err err = GorillaCron.startBlowJob(this);
-
-            if (err != null)
-            {
-                Log.e("startBlowJob failed!");
-            }
-        }
+        GorillaBase.startCronJob();
 
         GomessHandler.startService();
 
         GorillaLocation.startService(this);
 
         GorillaNetwork.logNetworkState();
+    }
+
+    public static void startCronJob()
+    {
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.M)
+        {
+            //
+            // Not available below Marshmallow.
+            //
+
+            return;
+        }
+
+        Context context = GorillaBase.getAppContext();
+
+        JobInfo.Builder builder = new JobInfo.Builder(47110815, new ComponentName(context, GorillaCron.class));
+        builder.setPersisted(true);
+
+        builder.setPeriodic((15 * 60 * 1000));
+
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
+        builder.setRequiresDeviceIdle(false);
+        builder.setRequiresCharging(false);
+
+        JobScheduler jobScheduler = context.getSystemService(JobScheduler.class);
+
+        if (jobScheduler == null)
+        {
+            Err.errp("JobScheduler system service unavailable");
+            return;
+        }
+
+        jobScheduler.schedule(builder.build());
+
+        int result = jobScheduler.schedule(builder.build());
+        if (result <= 0)
+        {
+            Err.errp("JobScheduler error=%d", result);
+            return;
+        }
+
+        Log.d("result=%d", result);
     }
 }
--- Modules/gorilla.service/src/main/java/com/aura/aosp/gorilla/service/GorillaCron.java
+++ Modules/gorilla.service/src/main/java/com/aura/aosp/gorilla/service/GorillaCron.java
@@ -2,54 +2,16 @@ package com.aura.aosp.gorilla.service;
 
 import android.support.annotation.RequiresApi;
 
-import android.content.ComponentName;
-import android.content.Context;
-
 import android.app.job.JobParameters;
-import android.app.job.JobScheduler;
 import android.app.job.JobService;
-import android.app.job.JobInfo;
 
 import android.os.Build;
 
-import com.aura.aosp.aura.common.simple.Err;
 import com.aura.aosp.aura.common.simple.Log;
 
 @RequiresApi(api = Build.VERSION_CODES.M)
 public class GorillaCron extends JobService
 {
-    public static Err startBlowJob(Context context)
-    {
-        JobInfo.Builder builder = new JobInfo.Builder(47110815, new ComponentName(context, GorillaCron.class));
-        builder.setPersisted(true);
-
-        builder.setPeriodic((15 * 60 * 1000));
-
-        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
-        builder.setRequiresDeviceIdle(false);
-        builder.setRequiresCharging(false);
-
-        JobScheduler jobScheduler = context.getSystemService(JobScheduler.class);
-
-        if (jobScheduler == null)
-        {
-            return Err.errp("JobScheduler system service unavailable");
-        }
-
-        jobScheduler.schedule(builder.build());
-
-        int result = jobScheduler.schedule(builder.build());
-        if (result <= 0)
-        {
-            return Err.errp("JobScheduler error=%d", result);
-
-        }
-
-        Log.d("result=%d", result);
-
-        return null;
-    }
-
     @Override
     public boolean onStartJob(JobParameters jobParameters)
     {
