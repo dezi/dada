640ce59841d515aeabf31c7d061c3c911e433b0e
dezi <dezi@kappa-mm.de>
2018-10-21 10:24:54 +0200

    Native stuff.

---
+++ Modules/aura.nat/src/main/cpp/libauranat.cpp
@@ -0,0 +1,9 @@
+#include <jni.h>
+
+extern "C" JNIEXPORT jbyteArray JNICALL Java_com_aura_aosp_aura_nat_levenshtein_levenshtein
+        (JNIEnv *env, jclass self, jbyteArray s1, jbyteArray s2)
+{
+    return 0;
+}
+
+
