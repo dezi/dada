021574617ee5c28a48437c042f70157629b95ec1
Dennis Zierahn <dezi@kappa-mm.de>
2018-10-22 11:18:29 +0200

    Hinting tuning.

--- Modules/gorilla.service/src/main/java/com/aura/aosp/gorilla/golang/GolangUtils.java
+++ Modules/gorilla.service/src/main/java/com/aura/aosp/gorilla/golang/GolangUtils.java
@@ -11,8 +11,7 @@ import com.aura.aosp.aura.common.simple.Simple;
 import java.io.File;
 import java.io.RandomAccessFile;
 
-import static java.lang.Math.min;
-
+@SuppressWarnings("unused")
 public class GolangUtils
 {
     private final static Object mutex = new Object();
@@ -154,7 +153,7 @@ public class GolangUtils
      * @param s2len number of bytes to use.
      * @return distance or null on error.
      */
-    public static Integer levenshtein(byte[] s1, int s1len, byte[] s2, int s2len)
+    public static Integer levenshteinOld(byte[] s1, int s1len, byte[] s2, int s2len)
     {
         if ((s1len > 48) || (s2len > 48))
         {
@@ -255,6 +254,113 @@ public class GolangUtils
     }
 
     /**
+     * Compute Levenshtein distance between two string in byte array form.
+     * <p>
+     * Recommended method if data comes from byte arrays because converting
+     * byte array to strings is expensive.
+     *
+     * @param s1    first byte array string.
+     * @param s1len number of bytes to use.
+     * @param s2    second byte array string.
+     * @param s2len number of bytes to use.
+     * @return distance or null on error.
+     */
+    public static Integer levenshtein(byte[] s1, int s1len, byte[] s2, int s2len)
+    {
+        if ((s1len > 48) || (s2len > 48))
+        {
+            Err.errp("string length too big s1=%s s2=%d", s1len, s2len);
+
+            return null;
+        }
+
+        //
+        // Convert byte array string into UTF-8 runes array.
+        //
+        // UTF-8 encoding:
+        //
+        // 0000 0000 – 0000 007F 	0xxxxxxx
+        // 0000 0080 – 0000 07FF 	110xxxxx 10xxxxxx
+        // 0000 0800 – 0000 FFFF 	1110xxxx 10xxxxxx 10xxxxxx
+        // 0001 0000 – 0010 FFFF 	11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+        //
+
+        int[] r1 = new int[s1len];
+        int[] r2 = new int[s2len];
+
+        int rows = 0;
+        int cols = 0;
+
+        for (int inx = 0; inx < s1len; inx++)
+        {
+            if ((rows > 0) && (s1[inx] & 0xc0) == 0x80)
+            {
+                r1[rows - 1] = (r1[rows - 1] << 8) + s1[inx];
+
+                continue;
+            }
+
+            r1[rows++] = s1[inx];
+        }
+
+        for (int inx = 0; inx < s2len; inx++)
+        {
+            if ((cols > 0) && (s2[inx] & 0xc0) == 0x80)
+            {
+                r2[cols - 1] = (r2[cols - 1] << 8) + s2[inx];
+
+                continue;
+            }
+
+            r2[cols++] = s2[inx];
+        }
+
+        int[] v0 = new int[cols + 1];
+        int[] v1 = new int[cols + 1];
+        int[] vt;
+
+        for (int i = 0; i <= cols; i++)
+        {
+            v0[i] = i;
+        }
+
+        int m1;
+        int dc;
+        int ic;
+        int sc;
+
+        for (int i = 0; i < rows; i++)
+        {
+            v1[0] = i + 1;
+
+            for (int j = 0; j < cols; j++)
+            {
+                dc = v0[j + 1] + 1;
+                ic = v1[j] + 1;
+
+                if (r1[i] == r2[j])
+                {
+                    sc = v0[j];
+                }
+                else
+                {
+                    sc = v0[j] + 1;
+                }
+
+                m1 = (dc < ic) ? dc : ic;
+
+                v1[j + 1] = m1 < sc ? m1 : sc;
+            }
+
+            vt = v0;
+            v0 = v1;
+            v1 = vt;
+        }
+
+        return v0[cols];
+    }
+
+    /**
      * Get real UTF-8 rune length from byte array.
      * <p>
      * This method is faster than a call to
@@ -306,7 +412,7 @@ public class GolangUtils
         /**
          * The phrase.
          */
-        String phrase;
+        final String phrase;
 
         /**
          * The score.
