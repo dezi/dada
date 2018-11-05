e3d45efacf81b2f353964e96318c524c5cf16ff9
Dennis Zierahn <dezi@kappa-mm.de>
2018-11-05 08:18:16 +0100

    Check in.

---
+++ pullall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ~/aura/dada; git pull; cd ..
+
+cd ~/aura/aura.java.com; git pull; cd ..
+cd ~/aura/aura.java.gui; git pull; cd ..
+cd ~/aura/aura.java.nlp; git pull; cd ..
+cd ~/aura/aura.java.ocv; git pull; cd ..
+
+cd ~/aura/gorilla.java.client; git pull; cd ..
+
+cd ~/aura/project.java.launcher; git pull; cd ..
+cd ~/aura/project.java.facedetect; git pull; cd ..
+cd ~/aura/project.java.actionsuggestions; git pull; cd ..
+cd ~/aura/project.java.textsuggestions; git pull; cd ..
+
+cd ~/go/src/github.com/auraproduct/aura.go.nlp; git pull; cd ..
+
+cd ~/go/src/github.com/auraproduct/project.go.kalditrainer; git pull; cd ..
+cd ~/go/src/github.com/auraproduct/project.go.textsuggestions; git pull; cd ..
