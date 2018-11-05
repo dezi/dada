a4dec9c05db0ba5350ef1fd3db343acbb638ebca
Dennis Zierahn <dezi@Denniss-MBP.localdomain>
2018-11-05 08:41:01 +0100

    Check in.

--- pullall.sh
+++ pullall.sh
@@ -6,6 +6,7 @@ cd ~/aura/aura.java.com; git pull; cd ..
 cd ~/aura/aura.java.gui; git pull; cd ..
 cd ~/aura/aura.java.nlp; git pull; cd ..
 cd ~/aura/aura.java.ocv; git pull; cd ..
+cd ~/aura/aura.java.sdk; git pull; cd ..
 
 cd ~/aura/gorilla.java.client; git pull; cd ..
 
@@ -13,6 +14,11 @@ cd ~/aura/project.java.launcher; git pull; cd ..
 cd ~/aura/project.java.facedetect; git pull; cd ..
 cd ~/aura/project.java.actionsuggestions; git pull; cd ..
 cd ~/aura/project.java.textsuggestions; git pull; cd ..
+
+cd ~/go/src/github.com/dezi/nlpstuff; git pull; cd ..
+
+cd ~/go/src/github.com/auraproduct/stuff.fid; git pull; cd ..
+cd ~/go/src/github.com/auraproduct/stuff.geo; git pull; cd ..
 
 cd ~/go/src/github.com/auraproduct/aura.go.nlp; git pull; cd ..
 
