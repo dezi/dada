9e6669d0788d524b38bbc6d685879467114321e7
Dennis Zierahn <dezi@kappa-mm.de>
2018-11-06 11:16:41 +0100

    Added delete atoms.

---
+++ pushall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ~/aura/dada; git push; cd ..
+
+cd ~/aura/aura.java.com; git push; cd ..
+cd ~/aura/aura.java.gui; git push; cd ..
+cd ~/aura/aura.java.nlp; git push; cd ..
+cd ~/aura/aura.java.ocv; git push; cd ..
+cd ~/aura/aura.java.sdk; git push; cd ..
+
+cd ~/aura/gorilla.java.client; git push; cd ..
+
+cd ~/aura/project.java.launcher; git push; cd ..
+cd ~/aura/project.java.facedetect; git push; cd ..
+cd ~/aura/project.java.actionsuggestions; git push; cd ..
+cd ~/aura/project.java.textsuggestions; git push; cd ..
+
+cd ~/go/src/github.com/dezi/nlpstuff; git push; cd ..
+
+cd ~/go/src/github.com/auraproduct/stuff.fid; git push; cd ..
+cd ~/go/src/github.com/auraproduct/stuff.geo; git push; cd ..
+
+cd ~/go/src/github.com/auraproduct/aura.go.nlp; git push; cd ..
+
+cd ~/go/src/github.com/auraproduct/project.go.kalditrainer; git pull; cd ..
+cd ~/go/src/github.com/auraproduct/project.go.textsuggestions; git pull; cd ..
